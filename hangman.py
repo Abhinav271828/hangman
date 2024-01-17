5aadbb96704c10bb6d5d4a546f75fb5b52e84de7
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-01-17 19:49:16 +0530

    Model code

--- hangman.py
+++ hangman.py
@@ -4,6 +4,7 @@ from torch.utils.data import Dataset, DataLoader
 from itertools import combinations
 from tqdm import tqdm
 from random import sample, uniform
+import math
 
 # This method generates roughly 2.5 million datapoints.
 def gen_data(filename):
@@ -51,6 +52,7 @@ def gen_data(filename):
                     #self.guesses.append(ord(c)-ord('a'))
     g.close()
 
+DEVICE = torch.device('mps:0')
 class HangmanData(Dataset):
     def __init__(self):
         try:
@@ -67,7 +69,7 @@ class HangmanData(Dataset):
             whole, word, guess = line[:-1].split(' ')
             # 26 is blank; 27 is pad
             self.words.append([ord(c)-ord('a') if c != '_' else 26 for c in word] + [27]*(29-len(word)))
-            self.guesses.append(ord(guess))
+            self.guesses.append(ord(guess)-ord('a'))
 
         print("Tensorifying")
         self.words = tensor(self.words)
@@ -79,15 +81,90 @@ class HangmanData(Dataset):
     def __len__(self):
         return self.words.__len__()
 
-#ds = HangmanData()
-#torch.save(ds, 'dataset.pkl')
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
 
 class HangmanModel(nn.Module):
     def __init__(self):
-        pass
+        super().__init__()
+        self.embedder = nn.Embedding(num_embeddings=28, embedding_dim=512, padding_idx=27)
+        self.pos_enc = PositionalEncoding(d_model=512)
+
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8, dim_feedforward=512, batch_first=True)
+        self.encoder = nn.TransformerEncoder(self.encoder_layer, 4)
+
+        self.head = nn.Sequential(nn.Linear(in_features=29*512, out_features=2048),
+                                  nn.Linear(in_features=2048, out_features=512),
+                                  nn.Linear(in_features=512, out_features=26))
 
     def forward(self, x):
-        pass
+        #x[0].to(DEVICE)
+        #x[1].to(DEVICE)
+        inputs, masks = x
+        embeddings = self.embedder(inputs)
+        positional_embeddings = self.pos_enc(embeddings)
+        encodings = self.encoder(src=positional_embeddings, mask=masks)
+        logits = self.head(encodings.flatten(1, 2))
+        return logits
 
-    def step(self):
-        pass
+    def step(self, dl, optim, loss_fn):
+        total_loss = 0
+        for batch in tqdm(dl, total=len(dl)):
+            inputs, guesses = batch
+            inputs = inputs.to(DEVICE)
+            guesses = guesses.to(DEVICE)
+            s_mask = (inputs != 27)
+            masks = torch.mul(s_mask.unsqueeze(2), s_mask.unsqueeze(1)).repeat(8, 1, 1)
+            logits = self((inputs, masks))
+            loss = loss_fn(logits, guesses)
+            loss.backward()
+            optim.step()
+            self.zero_grad()
+            total_loss += loss.item()
+        return total_loss / len(dl)
+    
+    def train_loop(self, dl):
+        lr = 0.1
+        optim = torch.optim.SGD(params=self.parameters(), lr=lr)
+        loss_fn = nn.CrossEntropyLoss(ignore_index=27)
+
+        min_loss = math.inf
+        count = 0
+        for e in tqdm(range(100)):
+            loss = self.step(dl, optim, loss_fn)
+            if loss < min_loss:
+                min_loss = loss
+                count = 0
+                torch.save(self, "model.pkz")
+            else:
+                count += 1
+
+            if count == 3: break
+
+#ds = HangmanData()
+#torch.save(ds, 'dataset.pkl')
+
+ds = torch.load('dataset.pkl')
+dl = DataLoader(ds, batch_size=128, shuffle=True)
+
+hm = HangmanModel()
+hm.train_loop(dl)
